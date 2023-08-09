e7a093819f2ce32297c05aff50577db223e1dfd6
emran <emranbspt@gmail.com>
2023-08-09 22:09:41 +0600

    delete extra files

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
+mp = {
+    'a':'green',
+    'b':'red',
+    'c':'yellow',
+    'd':'blue',
+    1:'green',
+    2:'red',
+    3:'yellow',
+    4:'blue',
+}
+
 $(document).keypress((event)=>{
     k = event.key;
-    for(v = 1; v<1000; v++){
+    for(v = 1; v<=1; v++){
         ar = [];
         for(i=0; i<v;i++){
             ar[i]=Math.floor((Math.random()*1000)%4) +1;
-        }
         
+        }
+        baja=(k)=>{
+            sn = new Audio('sounds/'+k+'.mp3');
+            sn.play();
+    
+        }
+        for(i=0; i<v;i++){
+            baja(ar[i])
+        }
+        console.log(ar)
+        $('.imm').click(()=>{
+
+
+        });
 
 
 
