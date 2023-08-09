a554ca69b6e3e33237d765dd26a34435276acf48
emran <emranbspt@gmail.com>
2023-08-09 21:54:37 +0600

    fix grid issues

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).keypress((event)=>{
+    k = event.key;
+    for(v = 1; v<1000; v++){
+        ar = [];
+        for(i=0; i<v;i++){
+            ar[i]=Math.floor((Math.random()*1000)%4) +1;
+        }
+        
+
+
+
+    }
+
+
+})
